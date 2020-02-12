3f686d9cc9bd62a74702ff1602f811be55d26aa2
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-12 09:21:50 +0100

    issues with checkbox switches

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -1,19 +1,30 @@
 <template>
     <div>
+        <b-row class="justify-content-md-center mt-5">
+            <b-col-md-6>
+                <b-card border-variant="success" header="The Query Window" align="center" header-bg-variant="success" header-text-variant="white">
+                    <b-card-text>Enjoy your day and be sure of the queries you make.</b-card-text>
 
-        <b-card border-variant="success" header="The Query Window" align="center">
-            <b-card-text>Enjoy your day, be sure of the query.</b-card-text>
-            <b-form @submit="onSubmit" @reset="onReset">
-                <!-- <b-form @submit="onSubmit" @reset="onReset" v-if="show"> -->
-                <b-form-group id="input-group-1" label-for="input-1" description="Ensure a button is on and a correct input is made to activate search button.">
-                    <b-form-input id="input-1" type="email" required placeholder="Enter email">
-                    </b-form-input>
-                    <b-form-checkbox-group :options="options" switches></b-form-checkbox-group>
-                </b-form-group>
-                <b-button type="submit" variant="primary">Submit</b-button>
-                <b-button type="reset" variant="danger">Reset</b-button>
-            </b-form>
-        </b-card>
+                    <b-form @submit="onSubmit" @reset="onReset">
+        <!-- <b-form @submit="onSubmit" @reset="onReset" v-if="show"> -->
+
+        <b-form-group id="query_pane" label-for="query_pane">
+                            <b-form-input id="input_area" placeholder="Enter a NIN, Issued Date or Last Name to make a query..."> </b-form-input>
+                            <span class="small" v-show="checki"> {{ warning }} </span>
+                            <b-form-checkbox-group class="limiter mt-3" v-model="option.checked" @change="updateBoxes" :value="option.value" :checked="option.checked" :disabled="option.disabled" switches></b-form-checkbox-group>
+                            <!-- <b-form-checkbox-group class="limiter mt-3" :options="options" switches></b-form-checkbox-group> -->
+                            <!-- <b-form-checkbox-group class="limiter mt-3" :options="options" :disabled="option.disabled" v-model="option.checked" @change="updateLimit" switches></b-form-checkbox-group> -->
+                        </b-form-group>
+
+                        <b-button class="mr-3" type="reset" variant="danger">Reset</b-button>
+
+                        <b-button class="ml-3" type="submit" variant="primary"
+                            :disabled="(checkboxSelected === false && fulfilledAllConditions == false || this.queried == true)"
+                            @click="checki">Search</b-button>
+                    </b-form>
+                </b-card>
+            </b-col-md-6>
+        </b-row>
     </div>
 </template>
 
@@ -22,27 +33,116 @@
     export default {
         data() {
             return {
-                options: [
-                { text: 'NIN', value: 'nin' },
-                { text: 'Issued Date', value: 'issued_date' },
-                // { text: 'Yellow (disabled)', value: 'yellow', disabled: true },
-                { text: 'Last Name', value: 'last_name' }
-                ]
+                tickLimit: 1,
+                ticks: [],
+                options: [{
+                        text: 'NIN',
+                        value: 'nin',
+                        checked: 'false',
+                        disabled: false
+                    },
+                    {
+                        text: 'Issued Date',
+                        value: 'issued_date',
+                        checked: 'false',
+                        disabled: false
+                    },
+                    {
+                        text: 'Last Name',
+                        value: 'last_name',
+                        checked: 'false',
+                        disabled: false
+                    }
+                ],
+                checked: '',
+                fulfilledAllConditions: false,
+                warning: ''
             }
         },
 
         methods: {
-            fx() {
-                return ("good")
+            checkboxSelected() {
+                if(this.checked > 1) {
+                    this.checked = false
+                }
+            },
+
+            checki() {
+                if (this.checkboxSelected === null && this.fulfilledAllConditions == false)
+                    prompt(this.message =
+                        "Ensure a button is on and a correct input is made to activate search button.")
             }
         },
 
         computed: {
-            selected() {
+            selectedsda() {
                 return ("I am a boy")
             }
+        },
+
+        watch: {
+        // button selection
+        // query link
+            makeQuery: {
+                immediate: true,
+                // main condition
+                handler() {
+                    this.checkboxSelected = false
+                }
+            }
         }
-    } 
+
+        // methods: {
+        //     updateLimit() {
+        //         if (this.ticks.length < this.tickLimit) {
+        //             // keep current selection and activate remaining options
+        //             this.boxes.forEach(box => {
+        //                 if (box.disabled) box.disabled = false;
+        //             });
+        //         }
+
+        //         if (this.ticks.length > this.tickLimit) {
+        //             // revert last selections (roughly) and update boxes based on new limit
+        //             let tickDiff = this.ticks.length - this.tickLimit;
+        //             for (var i = this.ticks.length - 1; i >= (this.ticks.length - tickDiff); i--) {
+        //                 this.boxes.find(box => {
+        //                     if (box.value == this.ticks[i].value) box.checked = false;
+        //                 })
+        //             };
+        //         }
+
+        //         this.updateBoxes();
+        //     },
+
+        //     updateBoxes() {
+        //         // update the number of ticks...
+        //         this.ticks = this.boxes.filter(box => box.checked);
+
+        //         // re-enable checkboxes if back under the limit...
+        //         if (this.ticks.length < this.tickLimit) {
+        //             this.boxes.forEach(box => {
+        //                 if (box.checked == false) box.disabled = false;
+        //             });
+        //         }
+
+        //         // disable empty checkboxes if at the limit...
+        //         if (this.ticks.length == this.tickLimit) {
+        //             this.boxes.forEach(box => {
+        //                 if (box.checked == false) box.disabled = true;
+        //             });
+        //         }
+        //     },
+
+        //     nukeTicks() {
+        //         this.boxes.forEach(box => {
+        //             box.checked = false;
+        //             box.disabled = false;
+        //         });
+        //         this.ticks = [];
+        //     }
+        // },
+    }
+
 </script>
 
 <style scoped>
