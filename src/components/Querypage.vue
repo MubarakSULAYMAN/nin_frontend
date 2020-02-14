59b3f1f7952e3264a38b699c61cef9a94b645c9c
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-14 21:42:23 +0100

    into to watch

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -3,7 +3,8 @@
         <b-row class="justify-content-md-center mt-5">
             <b-col md="6">
                 <b-form-group label="Inline switch style checkboxes">
-                    <b-form-checkbox-group :options="options" @change="checked" switches>
+                    <b-form-checkbox-group :options="options" switches>
+                        <!-- <b-form-checkbox-group :options="options" @change="checked" switches> -->
                     </b-form-checkbox-group>
                 </b-form-group>
             </b-col>
@@ -40,34 +41,30 @@
             }
         },
 
-        methods: {
-            checked(arrayItems) {
-                this.options.checked = true
-                this.options.disabled = true
-                this.selectedIndex = arrayItems.toString()
+        // methods: {
+        //     checked(arrayItems) {
+        //         this.options.checked = true
+        //         this.options.disabled = true
+        //         this.selectedIndex = arrayItems.toString()
+        //         // eslint-disable-next-line no-console
+        //         console.log(this.selectedIndex)
+
+        //         // // eslint-disable-next-line no-console
+        //         // console.log(this.options)
+
+        //         this.options.forEach(function (arrayCollection) {
+        //             let x = arrayCollection.value
+
+        //             // eslint-disable-next-line no-console
+        //             console.log("Array: " + x)
+        //         })
+        //     }
+        // },
+
+        watch: {
+            checked: function () {
                 // eslint-disable-next-line no-console
-                console.log(this.selectedIndex)
-
-                // // eslint-disable-next-line no-console
-                // console.log(this.options)
-
-                this.options.forEach(function (arrayCollection) {
-                    let x = arrayCollection.value
-
-                    // eslint-disable-next-line no-console
-                    console.log("Array: " + x)
-
-                    // eslint-disable-next-line no-console
-                    // console.log(x.includes('last_name'));
-
-                    // if (x.includes('last_name')) {
-                    //     // let y = x.slice('last_name')
-                    //     // eslint-disable-next-line no-console
-                    //     console.log(x);
-
-                    // }
-                })
-
+                console.log("Hello")
             }
         }
     }
