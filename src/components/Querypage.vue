a083963a1ce085d8ce3a7382cff20998b0f4bd51
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-14 17:51:58 +0100

    stucked

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -3,11 +3,9 @@
         <b-row class="justify-content-md-center mt-5">
             <b-col md="6">
                 <b-form-group label="Inline switch style checkboxes">
-                    <b-form-checkbox-group :options="options" @change="checked" v-bind="index in options" :disabled="checked === null" switches>
-                         <!-- :disabled="checked.length !== options.length" -->
+                    <b-form-checkbox-group :options="options" @change="checked" switches>
                     </b-form-checkbox-group>
                 </b-form-group>
-                <!-- :disabled="selectedIndex === null || answered" -->
             </b-col>
         </b-row>
     </div>
@@ -19,67 +17,59 @@
     export default {
         data() {
             return {
-                index: [],
                 selectedIndex: null,
-                others: null,
                 options: [{
                         text: 'NIN',
                         value: 'nin',
                         name: 'nin',
-                        disabled: false,
-                        checked: false
+                        disabled: false
                     },
                     {
                         text: 'Issued Date',
                         value: 'issued_date',
                         name: 'issued_date',
-                        disabled: false,
-                        checked: false
+                        disabled: false
                     },
                     {
                         text: 'Last Name',
                         value: 'last_name',
                         name: 'last_name',
-                        disabled: false,
-                        checked: false
+                        disabled: false
                     }
                 ],
             }
         },
 
         methods: {
-            checked(index) {
-                // this.options.checked = true
-                this.selectedIndex = index
+            checked(arrayItems) {
+                this.options.checked = true
+                this.options.disabled = true
+                this.selectedIndex = arrayItems.toString()
                 // eslint-disable-next-line no-console
                 console.log(this.selectedIndex)
-                
-                this.options.forEach(function(arrayCollection)
-                {
+
+                // // eslint-disable-next-line no-console
+                // console.log(this.options)
+
+                this.options.forEach(function (arrayCollection) {
+                    let x = arrayCollection.value
+
                     // eslint-disable-next-line no-console
-                    console.log(arrayCollection.value)
+                    console.log("Array: " + x)
+
+                    // eslint-disable-next-line no-console
+                    // console.log(x.includes('last_name'));
+
+                    // if (x.includes('last_name')) {
+                    //     // let y = x.slice('last_name')
+                    //     // eslint-disable-next-line no-console
+                    //     console.log(x);
+
+                    // }
                 })
-                // let a = [
-                //             this.options[0].value,
-                //             this.options[1].value,
-                //             this.options[2].value
-                //          ]
 
-                // if (this.selectedIndex === a['']) {
-                //     // eslint-disable-next-line no-console
-                //     console.log("Hello")
-                // }
-                
-
-                // let x = this.inde
-                // eslint-disable-next-line no-console
-                // console.log(x)
             }
-        },
+        }
     }
 
 </script>
-
-<style scoped>
-
-</style>
