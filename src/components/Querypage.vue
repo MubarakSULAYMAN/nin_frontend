473d3628f35990f8aa1e0932f7f169b47221a199
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-17 10:48:58 +0100

    if condition modified

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -3,7 +3,7 @@
         <div>
             <div v-text="selectedOption"></div>
             <ul>
-                <li v-for="(option, index) in options" :key="option.value" @change="checked($refs, $event, index)">
+                <li v-for="option in options" :key="option.value" @change="checked">
                     <input type="checkbox" ref="optionRef" :id="option.name" :value="option.value"
                         v-model="selectedOption">
                     <label :for="option.name">{{option.text}}</label>
@@ -44,25 +44,45 @@
         },
         methods: {
             checked() {
-                if (this.selectedOption[0] === this.$refs.optionRef[0].value) {
-                    return (this.$refs.optionRef[1].disabled = true, this.$refs.optionRef[2].disabled = true)
-                }
+                let selector1 = this.selectedOption[0]
 
-                else if (this.selectedOption[0] === this.$refs.optionRef[1].value) {
-                    return (this.$refs.optionRef[0].disabled = true, this.$refs.optionRef[2].disabled = true)
-                }
+                let ref1 = this.$refs.optionRef[0]
+                let ref2 = this.$refs.optionRef[1]
+                let ref3 = this.$refs.optionRef[2]
 
-                else if (this.selectedOption[0] === this.$refs.optionRef[2].value) {
-                    return (this.$refs.optionRef[0].disabled = true, this.$refs.optionRef[1].disabled = true)
-                }
-                
-                else {
+                let checkbox1 = (selector1 === ref1.value)
+                let checkbox2 = (selector1 === ref2.value)
+                let checkbox3 = (selector1 === ref3.value)
+
+                // let route = 123.456.7890
+
+                if (checkbox1) {
+                    return (ref2.disabled = true, ref3.disabled = true)
+                } else if (checkbox2) {
+                    return (ref1.disabled = true, ref3.disabled = true)
+                } else if (checkbox3) {
+                    return (ref1.disabled = true, ref2.disabled = true)
+                }  else if ( !checkbox1  ||  checkbox2  ||  !checkbox3) {
+                    // eslint-disable-next-line no-console
+                    console.log("Make a selection please.")
+                } else {
                     // eslint-disable-next-line no-console
                     console.log("A selection is required")
                 }
             },
+
             clear() {
                 this.selectedOption = []
+            },
+
+            submitRequest(e) {
+                this.todos.push({
+                    text: this.text,
+                    completed: false
+                });
+
+                // To prevent the request from submitting
+                e.preventDefault();
             }
         }
     }
