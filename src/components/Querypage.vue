0de7c1ab22598c6acfeac12064a7c1261e8f085c
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-17 03:11:15 +0100

    no lost

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -3,7 +3,7 @@
         <div>
             <div v-text="selectedOption"></div>
             <ul>
-                <li v-for="option in options" :key="option.value" @change="checked">
+                <li v-for="option in options" :key="option.value" @change="checked($refs, $event)">
                     <input type="checkbox" ref="" :id="option.name" :value="option.value" v-model="selectedOption"
                         @update:li="updatedLi">
                     <label :for="option.name">{{option.text}}</label>
@@ -44,7 +44,7 @@
             }
         },
         methods: {
-            checked() {
+            checked(event) {
                 this.$emit('update:li', this.selectedOption)
                 // if(this.selectedOption.value === this.options[0].value) {
 
@@ -54,14 +54,21 @@
                 // eslint-disable-next-line no-console
                 // console.log(this.selectedOption)
                 // eslint-disable-next-line no-console
-                console.log(this.$refs[''])
+                // console.log(this.$refs[''])
 
                 // }
                 // eslint-disable-next-line no-console
                 // console.log(this.options[1].value)
 
-                // eslint-disable-next-line no-console
-                // console.log(this.options[2].value)
+                // if(this.$refs[0].$refs[0].change()) {
+                    // eslint-disable-next-line no-console
+                    console.log(this.$refs)
+                    
+                    // eslint-disable-next-line no-console
+                    console.log(event)
+                // }
+
+                // this.$refs.checkbox.change()
             },
             updatedLi(value) {
                 // eslint-disable-next-line no-console
@@ -72,7 +79,9 @@
             }
         },
         mounted() {
-            
+            // eslint-disable-next-line no-console
+            // console.log(this.$refs['nin'])
+
         }
     }
 
