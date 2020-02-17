f75ceb1cbc64c12839a2536b2ba494d21d8cb998
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-17 14:32:15 +0100

    all reset button added, queryTerm condition added, no bug issues

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -1,37 +1,17 @@
 <template>
-    <div class="justify-content-md-center mt-5">
+    <div>
+        <input v-model="queryTerm" placeholder="Search...">
+        <p> Searching for <b>{{ queryTerm }}</b> from our records. </p>
         <div v-text="selectedOption"></div>
-        <b-row>
-            <b-col>
-                <b-form-group label="Inline switch style checkboxes">
-                    <span md="6" v-for="option in options" :key="option.value" @change="checked">
-                        <input type="checkbox" ref="optionRef" :id="option.name" :value="option.value"
-                            v-model="selectedOption">
-                        <label :for="option.name">{{option.text}}</label>
-                    </span>
-                    <div class="justify-content-md-center">
-                        <button @click="reset">Reset</button>
-                    </div>
-                    
-
-                </b-form-group>
-            </b-col>
-        </b-row>
-
+        <span v-for="option in options" :key="option.value" @change="checked">
+            <input type="checkbox" ref="optionRef" :id="option.name" :value="option.value" v-model="selectedOption">
+            <label :for="option.name"> {{option.text}} </label>
+        </span>
         <div>
-            <b-button>Reset Checkbox</b-button>
-            <b-button variant="danger">Reset ALl</b-button>
-            <b-button variant="success">Send Request</b-button>
+            <button @click="resetCheckbox"> Reset Checkbox </button>
+            <button @click="resetAll"> Reset All </button>
+            <button @click="submitRequest"> Make Query </button>
         </div>
-
-        <!-- <b-row class="justify-content-md-center mt-5">
-            <b-col md="6">
-                <b-form-group label="Inline switch style checkboxes">
-                    <b-form-checkbox-group :options="options" @change="checked" switches>
-                    </b-form-checkbox-group>
-                </b-form-group>
-            </b-col>
-        </b-row> -->
 
         <div v-if="checked"> {{startMessage}} </div>
     </div>
@@ -43,6 +23,7 @@
     export default {
         data() {
             return {
+                queryTerm: '',
                 startMessage: '',
                 selectedOption: [],
                 options: [{
@@ -67,7 +48,7 @@
             }
         },
         methods: {
-            
+
             checked() {
                 let startMessage = "A selection is required to begin a query."
                 let selector1 = this.selectedOption[0]
@@ -79,10 +60,6 @@
                 let checkbox1 = (selector1 === ref1.value)
                 let checkbox2 = (selector1 === ref2.value)
                 let checkbox3 = (selector1 === ref3.value)
-
-                // let disabled1  = (ref1.disabled = true)
-                // let disabled2  = (ref2.disabled = true)
-                // let disabled3  = (ref3.disabled = true)
 
                 // let route = 123.456.7890
 
@@ -98,27 +75,41 @@
                 }
             },
 
-            reset(e) {
+            resetCheckbox(e) {
                 let ref1 = this.$refs.optionRef[0]
                 let ref2 = this.$refs.optionRef[1]
                 let ref3 = this.$refs.optionRef[2]
 
                 e.preventDefault()
                 this.selectedOption = []
-                ref1.disabled =false
-                ref2.disabled =false
-                ref3.disabled =false
+                ref1.disabled = false
+                ref2.disabled = false
+                ref3.disabled = false
+            },
+
+            resetAll(e) {
+                let ref1 = this.$refs.optionRef[0]
+                let ref2 = this.$refs.optionRef[1]
+                let ref3 = this.$refs.optionRef[2]
+
+                e.preventDefault()
+                this.queryTerm = ''
+                this.selectedOption = []
+                ref1.disabled = false
+                ref2.disabled = false
+                ref3.disabled = false
 
             },
 
             submitRequest(e) {
-                this.todos.push({
-                    text: this.text,
-                    completed: false
-                });
-
-                // To prevent the request from submitting
                 e.preventDefault();
+                if(this.checked && this.queryTerm != '') {
+                    // eslint-disable-next-line no-console
+                    console.log("We are good to go.")
+                } else {
+                    // eslint-disable-next-line no-console
+                    console.log("Check a checkbox and input a valid query term to begin a query.")
+                }
             }
         }
     }
