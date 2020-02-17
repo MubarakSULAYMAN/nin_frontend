d9fea461371a0e78b3c8b440a91e045ba80c3c7a
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-17 14:01:21 +0100

    reset working completely

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -1,16 +1,37 @@
 <template>
-    <div>
+    <div class="justify-content-md-center mt-5">
+        <div v-text="selectedOption"></div>
+        <b-row>
+            <b-col>
+                <b-form-group label="Inline switch style checkboxes">
+                    <span md="6" v-for="option in options" :key="option.value" @change="checked">
+                        <input type="checkbox" ref="optionRef" :id="option.name" :value="option.value"
+                            v-model="selectedOption">
+                        <label :for="option.name">{{option.text}}</label>
+                    </span>
+                    <div class="justify-content-md-center">
+                        <button @click="reset">Reset</button>
+                    </div>
+                    
+
+                </b-form-group>
+            </b-col>
+        </b-row>
+
         <div>
-            <div v-text="selectedOption"></div>
-            <ul>
-                <li v-for="option in options" :key="option.value" @change="checked">
-                    <input type="checkbox" ref="optionRef" :id="option.name" :value="option.value"
-                        v-model="selectedOption">
-                    <label :for="option.name">{{option.text}}</label>
-                </li>
-            </ul>
-            <button @click="reset">Reset</button>
+            <b-button>Reset Checkbox</b-button>
+            <b-button variant="danger">Reset ALl</b-button>
+            <b-button variant="success">Send Request</b-button>
         </div>
+
+        <!-- <b-row class="justify-content-md-center mt-5">
+            <b-col md="6">
+                <b-form-group label="Inline switch style checkboxes">
+                    <b-form-checkbox-group :options="options" @change="checked" switches>
+                    </b-form-checkbox-group>
+                </b-form-group>
+            </b-col>
+        </b-row> -->
 
         <div v-if="checked"> {{startMessage}} </div>
     </div>
@@ -46,6 +67,7 @@
             }
         },
         methods: {
+            
             checked() {
                 let startMessage = "A selection is required to begin a query."
                 let selector1 = this.selectedOption[0]
@@ -58,18 +80,18 @@
                 let checkbox2 = (selector1 === ref2.value)
                 let checkbox3 = (selector1 === ref3.value)
 
-                let disabled1  = (ref1.disabled = true)
-                let disabled2  = (ref2.disabled = true)
-                let disabled3  = (ref3.disabled = true)
+                // let disabled1  = (ref1.disabled = true)
+                // let disabled2  = (ref2.disabled = true)
+                // let disabled3  = (ref3.disabled = true)
 
                 // let route = 123.456.7890
 
                 if (checkbox1) {
-                    return (disabled2, disabled3)
+                    return (ref2.disabled = true, ref3.disabled = true)
                 } else if (checkbox2) {
-                    return (disabled1, disabled3)
+                    return (ref1.disabled = true, ref3.disabled = true)
                 } else if (checkbox3) {
-                    return (disabled1, disabled2)
+                    return (ref1.disabled = true, ref2.disabled = true)
                 } else {
                     // eslint-disable-next-line no-console
                     return startMessage
@@ -77,8 +99,16 @@
             },
 
             reset(e) {
+                let ref1 = this.$refs.optionRef[0]
+                let ref2 = this.$refs.optionRef[1]
+                let ref3 = this.$refs.optionRef[2]
+
                 e.preventDefault()
                 this.selectedOption = []
+                ref1.disabled =false
+                ref2.disabled =false
+                ref3.disabled =false
+
             },
 
             submitRequest(e) {
