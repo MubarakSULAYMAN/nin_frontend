55181cf1b916f74cec1ffd63dd9c34030bd1f6db
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-02-18 13:33:12 +0100

    checkbox changed to radiobutton, disabling funtion removed, other functions modified

--- src/components/Querypage.vue
+++ src/components/Querypage.vue
@@ -1,19 +1,20 @@
 <template>
     <div>
-        <input v-model="queryTerm" placeholder="Search...">
-        <p> Searching for <b>{{ queryTerm }}</b> from our records. </p>
-        <div v-text="selectedOption"></div>
-        <span v-for="option in options" :key="option.value">
-            <input type="checkbox" ref="optionRef" :id="option.name" :value="option.value" v-model="selectedOption">
-            <label :for="option.name"> {{option.text}} </label>
+        <input v-model='queryTerm' placeholder='Search...'>
+        <p v-if='!searching'> Search for <b>{{ queryTerm }}</b> from our records. </p>
+        <p v-else> Searching for <b>{{ queryTerm }}</b> from our records. </p>
+        <div v-text='selectedOption'></div>
+        <span v-for='option in options' :key='option.value'>
+            <input type='radio' :id='option.name' name='eradio' :value='option.value' v-model='selectedOption'>
+            <label :for='option.name'> {{option.text}} </label>
         </span>
         <div>
-            <button @click="resetCheckbox"> Reset Checkbox </button>
-            <button @click="resetAll"> Reset All </button>
-            <button @click="verifyInput"> Make Query </button>
+            <button @click='resetCheckbox'> Reset Checkbox </button>
+            <button @click='resetAll'> Reset All </button>
+            <button @click='submitForm'> Make Query </button>
         </div>
 
-        <!-- <div v-if="checked"> {{startMessage}} </div> -->
+        <!-- <div v-if='checked'> {{startMessage}} </div> -->
     </div>
 </template>
 
@@ -21,15 +22,17 @@
 
 <script>
     let validNIN = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
-    let validIssuedDate = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
-    let validLastName = /^[a-zA-Z]{2,40}$/;
+    // let validIssuedDate = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
+    // let validLastName = /^[a-zA-Z]{2,40}$/;
 
     export default {
         data() {
             return {
                 queryTerm: '',
                 startMessage: 'A selection is required to begin a query.',
-                selectedOption: [],
+                selectedOption: '',
+
+                searching: false,
 
                 options: [{
                         text: 'NIN',
@@ -54,79 +57,103 @@
         },
         methods: {
 
-            checked(e) {
-                e.preventDefault()
+            // checked(e) {
+            //     e.preventDefault()
 
-                let selector1 = this.selectedOption[0]
+            //     let selector1 = this.selectedOption[0]
 
-                let ref1 = this.$refs.optionRef[0]
-                let ref2 = this.$refs.optionRef[1]
-                let ref3 = this.$refs.optionRef[2]
+            //     let ref1 = this.$refs.optionRef[0]
+            //     let ref2 = this.$refs.optionRef[1]
+            //     let ref3 = this.$refs.optionRef[2]
 
-                let checkbox1 = (selector1 === ref1.value)
-                let checkbox2 = (selector1 === ref2.value)
-                let checkbox3 = (selector1 === ref3.value)
+            //     let checkbox1 = (selector1 === ref1.value)
+            //     let checkbox2 = (selector1 === ref2.value)
+            //     let checkbox3 = (selector1 === ref3.value)
 
-                if (checkbox1) {
-                    // eslint-disable-next-line no-console
-                    console.log("NIN is selected.")
-                    return (ref2.disabled = true, ref3.disabled = true, true)
-                } else if (checkbox2) {
-                    // eslint-disable-next-line no-console
-                    console.log("Issued Date is selected.")
-                    return (ref1.disabled = true, ref3.disabled = true, true)
-                } else if (checkbox3) {
-                    // eslint-disable-next-line no-console
-                    console.log("Last Name is selected.")
-                    return (ref1.disabled = true, ref2.disabled = true, true)
-                } else {
-                    // eslint-disable-next-line no-console
-                    console.log("No selection...")
-                    return false
-                }
-            },
+            //     if (checkbox1) {
+            //         // eslint-disable-next-line no-console
+            //         console.log('NIN is selected.')
+            //         return (ref2.disabled = true, ref3.disabled = true, true)
+            //     } else if (checkbox2) {
+            //         // eslint-disable-next-line no-console
+            //         console.log('Issued Date is selected.')
+            //         return (ref1.disabled = true, ref3.disabled = true, true)
+            //     } else if (checkbox3) {
+            //         // eslint-disable-next-line no-console
+            //         console.log('Last Name is selected.')
+            //         return (ref1.disabled = true, ref2.disabled = true, true)
+            //     } else {
+            //         // eslint-disable-next-line no-console
+            //         console.log('No selection...')
+            //         return false
+            //     }
+            // },
 
             resetCheckbox(e) {
-                let ref1 = this.$refs.optionRef[0]
-                let ref2 = this.$refs.optionRef[1]
-                let ref3 = this.$refs.optionRef[2]
+                // let ref1 = this.$refs.optionRef[0]
+                // let ref2 = this.$refs.optionRef[1]
+                // let ref3 = this.$refs.optionRef[2]
 
                 e.preventDefault()
-                this.selectedOption = []
-                ref1.disabled = false
-                ref2.disabled = false
-                ref3.disabled = false
+                this.selectedOption = ''
+                // ref1.disabled = false
+                // ref2.disabled = false
+                // ref3.disabled = false
             },
 
             resetAll(e) {
-                let ref1 = this.$refs.optionRef[0]
-                let ref2 = this.$refs.optionRef[1]
-                let ref3 = this.$refs.optionRef[2]
+                // let ref1 = this.$refs.optionRef[0]
+                // let ref2 = this.$refs.optionRef[1]
+                // let ref3 = this.$refs.optionRef[2]
 
                 e.preventDefault()
                 this.queryTerm = ''
-                this.selectedOption = []
-                ref1.disabled = false
-                ref2.disabled = false
-                ref3.disabled = false
+                this.selectedOption = ''
+                // ref1.disabled = false
+                // ref2.disabled = false
+                // ref3.disabled = false
 
             },
 
-            verifyForm(e) {
-                e.preventDefault();
-
-                let myQuery = this.queryTerm
-
-                if (this.checked(e) && myQuery != '') {
-                    // eslint-disable-next-line no-console
-                    console.log("We are good to go with the that.")
-                    return true
+            submitForm() {
+                if (this.queryTerm === '' && this.selectedOption === '') {
+                    alert('Please select an option and enter a query term.')
+                } else if (this.selectedOption === '') {
+                    alert('Please select an option.')
+                } else if (this.queryTerm === '') {
+                    alert('Please enter a query term.')
                 } else {
-                    // eslint-disable-next-line no-console
-                    console.log("Check a checkbox and input a valid query term to begin a query.")
-                    return false
+                    let url = ''
+
+                    if (this.selectedOption === 'nin') {
+                        if (!this.queryTerm.match(validNIN)) {
+                            alert('Invalid NIN')
+                            return;
+                        }
+
+                        url = 'xyz.com/' + this.queryTerm
+                    }
+
+                    alert('The url is ' + url);
+                    this.searching = true;
                 }
             },
+
+            // verifyForm(e) {
+            //     e.preventDefault();
+
+            //     let myQuery = this.queryTerm
+
+            //     if (this.checked(e) && myQuery != '') {
+            //         // eslint-disable-next-line no-console
+            //         console.log('We are good to go with the that.')
+            //         return true
+            //     } else {
+            //         // eslint-disable-next-line no-console
+            //         console.log('Check a checkbox and input a valid query term to begin a query.')
+            //         return false
+            //     }
+            // },
 
             verifyInput(e) {
                 e.preventDefault()
@@ -137,51 +164,51 @@
 
                     if (myQuery.match(validNIN)) {
                         // eslint-disable-next-line no-console
-                        console.log("NIN is good to go.")
+                        console.log('NIN is good to go.')
                         return true
                     } else {
                         // eslint-disable-next-line no-console
-                        console.log("NIN is invalid.")
+                        console.log('NIN is invalid.')
                         return false
                     }
-                } else if (this.checked(e).checkbox2) {
+                    // } else if (this.checked(e).checkbox2) {
 
-                    let myQuery = this.queryTerm
+                    //     let myQuery = this.queryTerm
 
-                    if (myQuery.match(validIssuedDate)) {
-                        // eslint-disable-next-line no-console
-                        console.log("Issued Date is good to go.")
-                        return true
-                    } else {
-                        // eslint-disable-next-line no-console
-                        console.log("Issued Date is invalid.")
-                        return true
-                    }
-                } else if (this.checked(e).checkbox3) {
+                    //     if (myQuery.match(validIssuedDate)) {
+                    //         // eslint-disable-next-line no-console
+                    //         console.log('Issued Date is good to go.')
+                    //         return true
+                    //     } else {
+                    //         // eslint-disable-next-line no-console
+                    //         console.log('Issued Date is invalid.')
+                    //         return true
+                    //     }
+                    // } else if (this.checked(e).checkbox3) {
 
-                    let myQuery = this.queryTerm
+                    //     let myQuery = this.queryTerm
 
-                    if (myQuery.match(validLastName)) {
-                        // eslint-disable-next-line no-console
-                        console.log("Last Name is good to go.")
-                        return true
-                    } else {
-                        // eslint-disable-next-line no-console
-                        console.log("Last Name is invalid.")
-                        return false
-                    }
-                } 
+                    //     if (myQuery.match(validLastName)) {
+                    //         // eslint-disable-next-line no-console
+                    //         console.log('Last Name is good to go.')
+                    //         return true
+                    //     } else {
+                    //         // eslint-disable-next-line no-console
+                    //         console.log('Last Name is invalid.')
+                    //         return false
+                    //     }
+                }
 
             },
 
-            submitRequest(e) {
-                e.preventDefault();
+            // submitRequest(e) {
+            //     e.preventDefault();
 
-                if (this.verifyInput) {
-                    // eslint-disable-next-line no-console
-                    console.log("Query will be successful.")
-                }
-            }
+            //     if (this.verifyInput) {
+            //         // eslint-disable-next-line no-console
+            //         console.log('Query will be successful.')
+            //     }
+            // }
         }
     }
 
