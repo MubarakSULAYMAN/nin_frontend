1dba33fc88a0c59d14b23c8230c1d55fff3b78cc
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2020-03-28 18:07:14 +0100

    modified api.js for production

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 var $api = axios.create({
-    baseURL: `http://127.0.0.1:5000/`,
+    baseURL: `https://record-api.herokuapp.com/`,
     withCredentials: false,
     headers: {
         'Accept': 'application/json',
@@ -9,7 +9,7 @@ var $api = axios.create({
         'Authorization': 'Bearer {token}',
         'Owner': 'Mubarak SULAYMAN',
         'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Origin': 'http://localhost:8080',
+        'Access-Control-Allow-Origin': 'https://record-api.herokuapp.com',
         "Allow": 'HEAD, OPTIONS, GET'
     }
 })
